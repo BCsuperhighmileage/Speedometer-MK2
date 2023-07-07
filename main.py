6335c68163f6128850f4fb04fc74666690b092e6
Clayton Leuck <clayton.leuck@gmail.com>
2023-07-07 14:19:18 -0400

    Final changes to UI & added odometer & lap counter

--- main.py
+++ main.py
@@ -89,6 +89,15 @@ def updates_slider(value):
     print('updates slider')
     print(value)
 
+def reset_odometer():
+    print('reset_odometer is still in production')
+
+def reset_lap_counter():
+    print('reset_lap_counter is still in production')
+
+
+
+
 
 # setting up the colors of the graph
 # view all the colors available for tkinter: http://cs111.wellesley.edu/archive/cs111_fall14/public_html/labs/lab12/tkintercolor.html
@@ -97,7 +106,7 @@ SECONDARY_COLOR = 'black'
 
 ### START of Setup
 window = tk.Tk()
-
+window.title("MK2-Speedometer")
 window.geometry("1400x1400")
 window.configure(bg=MAIN_COLOR) # default is dark gray
 
@@ -183,54 +192,108 @@ framer.place(relx=0, rely=0.6, relwidth=1, relheight=0.5)
 control_center_frame = ctk.CTkFrame(framer)
 control_center_frame.grid(row=0, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-# create the main label for the info box
+# create the main label for the control center
 control_center_frame_main_text = ctk.CTkLabel(master=control_center_frame, text="Control Center")
 control_center_frame_main_text.grid(row=0, column=0, columnspan=1, padx=10, pady=10)
 
 
-# buttons for the controls
+
+
+
+# FIRST COLUMN
+# Debug Mode
 graph_button_1 = ctk.CTkButton(master=control_center_frame, border_width=2, text="Debug Mode", command=debug_mode)
 graph_button_1.grid(row=1, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-fake_data_switch = ctk.CTkSwitch(master=control_center_frame, border_width=2, text="Input Fake Data", command=input_fake_data)
-fake_data_switch.grid(row=2, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
-
-
-
-
-# graph control of buttons
+# Reset Graph
 graph_button_1 = ctk.CTkButton(master=control_center_frame, border_width=2, text="Reset Graph", command=reset_graph)
-graph_button_1.grid(row=0, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+graph_button_1.grid(row=2, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-documentation_button = ctk.CTkButton(master=control_center_frame, border_width=2, text="Documentation", command=documentation_link) # TODO link to Github README
-documentation_button.grid(row=1, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
-# Basic Function Buttons
+# Exit Button
 exit_button = ctk.CTkButton(master=control_center_frame, border_width=2, text="Exit", command=exit_function)
-exit_button.grid(row=0, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
+exit_button.grid(row=3, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
+# Reset Button
 reset_button = ctk.CTkButton(master=control_center_frame, border_width=2, text="Reset Program", command=reset_program)
-reset_button.grid(row=1, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
+reset_button.grid(row=4, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
 
-# slider 1
+
+
+
+# SECOUND COLUMN
+# Documentation Link Button
+documentation_button = ctk.CTkButton(master=control_center_frame, border_width=2, text="Documentation", command=documentation_link) # TODO link to Github README
+documentation_button.grid(row=1, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+# Reset Odometer Button
+reset_odometer = ctk.CTkButton(master=control_center_frame, border_width=2, text="Reset Odometer", command=reset_odometer)
+reset_odometer.grid(row=2, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+# Reset Lap Counter Button
+reset_odometer = ctk.CTkButton(master=control_center_frame, border_width=2, text="Reset Lap Counter", command=reset_lap_counter)
+reset_odometer.grid(row=3, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+# Input Fake Data Switch
+fake_data_switch = ctk.CTkSwitch(master=control_center_frame, border_width=2, text="Input Fake Data", command=input_fake_data)
+fake_data_switch.grid(row=4, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+
+
+# THIRD COLUMN
+# MPH Slider and Label
 max_mph_slider_label = ctk.CTkLabel(master=control_center_frame, text="Maximum MPH(1-50)")
-max_mph_slider_label.grid(row=4, column=1, columnspan=1, padx=10, pady=10)
+max_mph_slider_label.grid(row=1, column=2, columnspan=1, padx=10, pady=10)
 
 max_mph_slider = ctk.CTkSlider(master=control_center_frame, width=160, height=16, border_width=5.5, from_=1, to=50, number_of_steps=49, command=mph_slider)
-max_mph_slider.grid(row=5, column=1, padx=(20, 10), pady=(10, 10), sticky="ew")
+max_mph_slider.grid(row=2, column=2, padx=(20, 10), pady=(10, 10), sticky="ew")
 
 
-
-# slider 2
+# Updates Per Minute Slider and Label
 updates_per_min_slider_label = ctk.CTkLabel(master=control_center_frame, text="Number of Updates per Minute")
-updates_per_min_slider_label.grid(row=4, column=2, columnspan=1, padx=10, pady=10)
+updates_per_min_slider_label.grid(row=3, column=2, columnspan=1, padx=10, pady=10)
 
 updates_per_min_slider = ctk.CTkSlider(master=control_center_frame, width=160, height=16, border_width=5.5, from_=0, to=500, number_of_steps=50, command=updates_slider)
-updates_per_min_slider.grid(row=5, column=2, padx=(20, 10), pady=(10, 10), sticky="ew")
+updates_per_min_slider.grid(row=4, column=2, padx=(20, 10), pady=(10, 10), sticky="ew")
 
+
+
+
+#FORTH COLUMN
+# create the label for the odometer
+control_center_odometer_label = ctk.CTkLabel(master=control_center_frame, text="Odometer")
+control_center_odometer_label.grid(row=1, column=3, columnspan=1, padx=10, pady=10)
+
+# Displays the current miles traveled in the format: 00.00 Laps
+miles_traveled = '00.00'
+control_center_odometer_output = tk.Label(master=control_center_frame, text=f"{miles_traveled} Miles", borderwidth=1, relief="solid")
+control_center_odometer_output.grid(row=2, column=3, columnspan=1, padx=10, pady=10)
+
+
+# create the label for the lap counter
+control_center_lap_counter_label = ctk.CTkLabel(master=control_center_frame, text="Lap Counter")
+control_center_lap_counter_label.grid(row=3, column=3, columnspan=1, padx=10, pady=10)
+
+
+# Displays the current lap in the format: 0.00 Laps
+current_lap = '00.0'
+control_center_lap_counter_output = tk.Label(master=control_center_frame, text=f"{current_lap} Laps", borderwidth=1, relief="solid")
+control_center_lap_counter_output.grid(row=4, column=3, columnspan=1, padx=10, pady=10)
+
+
+
+
+#Fifth Column
+# create the label for the lap counter input box
+control_center_lap_counter_input = ctk.CTkLabel(master=control_center_frame, text="Enter the length of a single lap in miles: .89 miles is 0.89")
+control_center_lap_counter_input.grid(row=1, column=4, columnspan=1, padx=10, pady=10)
+
+
+# Input box for length of lap
+entry = ctk.CTkEntry(master=control_center_frame, placeholder_text="Input: 0.00")
+entry.grid(row=2, column=4, padx=(20, 20), pady=(20, 20), sticky="nsew")
 ### END of Control Center
-
 
 
 
